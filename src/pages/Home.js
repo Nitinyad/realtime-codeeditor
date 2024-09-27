b19e27c00a302a7c073fe1a074cd1910d054c5e8
Nitinyad <92179118+Nitinyad@users.noreply.github.com>
2024-09-27 09:33:09 +0530

    commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,70 +1,3 @@
-// import React, { useState } from 'react'
-// import {v4 as uuidV4} from 'uuid'
-// import toast from 'react-hot-toast';
-// import { useNavigate } from 'react-router-dom';
-
-// const Home = () => {
-//   const navigate = useNavigate()
-//   const [roomId , setRoomId] = useState('');
-//   const [username , setUsername] = useState('')
-
-//   const createNewRoom = (e) =>{
-//     e.preventDefault();
-//     const id = uuidV4();
-//     setRoomId(id);
-//     toast.success("Created New Room")
-//   }
-//   const joinRoom = () =>{
-//     if(!roomId || !username){
-//       toast.error("RoomId & username is required");
-//       return;
-//     }
-//     //redirect
-//     navigate(`/editor/${roomId}`,{
-//       state:{
-//         username,
-//       }
-//     })
-//   }
-//   const handleInputEnter = (e) =>{
-//     if(e.code === 'Enter'){
-//       joinRoom();
-//     }
-//   }
-//   return (
-//     <div className='homePageWrapper'>
-//       <div className="formWrapper">
-//         <img className='homePageLogo' src="/code.png" alt="logo" />
-//         <h4 className='mainLabel'>Paste your invitation RoomID</h4>
-//         <div className="inputGroup">
-//           <input type="text" className='inputBox' placeholder='Room ID' 
-//             onChange={(e)=>setRoomId(e.target.value)}
-//             value={roomId}
-//             onKeyUp={handleInputEnter}
-//           />
-//           <input type="text" className='inputBox' placeholder='Username'
-//             onChange={(e)=>setUsername(e.target.value)}
-//             value={username}
-//             onKeyUp={handleInputEnter}
-//           />
-//           <button className='btn joinBtn' onClick={joinRoom}>Join</button>
-//           <span className='createInfo'>
-//             If you don't have an invite then create &nbsp;
-//             <a onClick={createNewRoom} href="." className='createNewBtn'>new room</a>
-//           </span>
-//         </div>
-//       </div>
-//       <footer>
-//         <h4>Built with ❤  by <a href="https://github.com/Nitinyad">Nitin</a></h4>
-//       </footer>
-//     </div>
-
-//   );
-
-// }
-
-// export default Home
-
 
 
 import React, { useState } from 'react';
@@ -145,10 +78,7 @@ const Home = () => {
                 </div>
             </div>
             <footer>
-                <h4>
-                    Built with 💛 &nbsp; by &nbsp;
-                    <a href="https://github.com/codersgyan">Coder's Gyan</a>
-                </h4>
+                <h4>Built with ❤  by <a href="https://github.com/Nitinyad">Nitin</a></h4>
             </footer>
         </div>
     );
